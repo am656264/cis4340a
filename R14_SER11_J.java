7159d72915d7f33b396ab3681db269ae1828d2d8
am656264 <am656264@ucf.edu>
2025-02-26 17:14:34 -0500

    Update and Fixed R14_SER11_J.java

--- R14_SER11_J.java
+++ R14_SER11_J.java
@@ -1,9 +1,19 @@
 //Rule 14. Serialization (SER)
 //Ex: SER11-J. Prevent overwriting of externalizable objects 
+private final Object lock = new Object();
+private boolean initialized = false;
+ 
 public void readExternal(ObjectInput in)
                          throws IOException, ClassNotFoundException {
-   // Read instance fields
-   this.name = (String) in.readObject();
-   this.UID = in.readInt();
-   // ...
+  synchronized (lock) {
+    if (!initialized) {
+      // Read instance fields
+      this.name = (String) in.readObject();
+      this.UID = in.readInt();
+      // ... 
+      initialized = true;
+    } else {
+      throw new IllegalStateException();
+    }
+  }
 }
