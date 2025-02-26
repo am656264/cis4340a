c2c35de5c18860a886a4d78933ccc228ea440a88
am656264 <am656264@ucf.edu>
2025-02-25 22:50:54 -0500

    Create R12_TSM00_J.java

---
+++ R12_TSM00_J.java
@@ -0,0 +1,13 @@
+//Rule 12. Thread-Safety Miscellaneous (TSM)
+//TSM00-J. Do not override thread-safe methods with methods that are not thread-safe
+class Base {
+  public synchronized void doSomething() {
+    // ...
+  }
+}
+ 
+class Derived extends Base {
+  @Override public void doSomething() {
+    // ...
+  }
+}
