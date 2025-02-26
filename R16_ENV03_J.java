fd835fa449f28b988fb4b8d1edae644a3c44f42a
am656264 <am656264@ucf.edu>
2025-02-26 17:22:52 -0500

    Update and Fixed R16_ENV03_J.java

--- R16_ENV03_J.java
+++ R16_ENV03_J.java
@@ -1,6 +1,7 @@
 //Rule 16. Runtime Environment (ENV)
 //Ex: ENV03-J. Do not grant dangerous combinations of permissions 
-// Grant the klib library AllPermission 
-grant codebase "file:${klib.home}/j2se/home/klib.jar" {
-  permission java.security.AllPermission;
+grant codeBase
+    "file:${klib.home}/j2se/home/klib.jar", signedBy "Admin" {
+  permission java.io.FilePermission "/tmp/*", "read";
+  permission java.io.SocketPermission "*", "connect";
 };
